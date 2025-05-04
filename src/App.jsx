876a75dbbdb7ba64e3004303764f0b2429b708bc
Ayushpandey18 <125882394+Ayushpandey18@users.noreply.github.com>
2025-05-05 04:13:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ function App() {
         },
       }
     );
+    setIsLoggedIn(false);
     document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     localStorage.clear();
